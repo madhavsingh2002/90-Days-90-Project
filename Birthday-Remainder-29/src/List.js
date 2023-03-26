34b8ab0b43174f0c1fd0416683fec736cb98f9ae
madhav singh <94055502+madhavsingh2002@users.noreply.github.com>
2023-03-26 14:37:22 +0530

    29

---
+++ Birthday-Remainder-29/src/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+const List =({people})=>{
+    return(
+        <>
+            {people.map((person)=>{
+                const {id, name, age, image} = person;
+                return(
+                    <article key={id} className='person' >
+                        <img src= {image} alt={name} />
+                        <div>
+                            <h3>{name} </h3>
+                            <p> {age} Years </p>
+                        </div>
+                    </article>
+                )
+            })}
+        </>
+    )
+}
+export default List;
